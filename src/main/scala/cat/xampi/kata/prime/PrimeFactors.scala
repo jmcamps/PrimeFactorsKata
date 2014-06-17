a6e4459c0b527daf9ecc0a99e0faacd0441bfdcd
Josep Maria Camps i Riba <jmcamps@gmail.com>
2014-06-17 12:10:12 +0200

    Impreative version

--- src/main/scala/cat/xampi/kata/prime/PrimeFactors.scala
+++ src/main/scala/cat/xampi/kata/prime/PrimeFactors.scala
@@ -2,10 +2,18 @@ package cat.xampi.kata.prime
 
 object PrimeFactors {
   def generate(number: Int): List[Int] = {
-    number match {
-      case 1 => List.empty
-      case x if x > 1 => List(number)
-    }
-  }
+    var result = List[Int]()
+    var actual = number
+    if (number > 1) {
+      for (denominator <- 2 to number) {
+        while (actual % denominator == 0 && actual / denominator > 1) {
+          actual = actual / denominator
+          result =  result ::: List(denominator)
+        }
 
+      } 
+      result =  result ::: List(actual)
+    }
+    result
+  } 
 }
--- src/test/scala/cat/xampi/kata/prime/PrimeFactorsSuite.scala
+++ src/test/scala/cat/xampi/kata/prime/PrimeFactorsSuite.scala
@@ -18,4 +18,9 @@ class PrimeFactorsSuite extends FunSuite{
   test("Prime factor for 3 should be List(3)") {
     assert(PrimeFactors.generate(3) === List(3)) 
   } 
+  
+  test("Prime factor for 4 should be List(2,2)") {
+    assert(PrimeFactors.generate(4) === List(2,2))  
+  } 
+  
 }
