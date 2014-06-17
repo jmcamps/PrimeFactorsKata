ff1326040e984566b89960edbb84b63509088588
Josep Maria Camps i Riba <jmcamps@gmail.com>
2014-06-17 12:29:08 +0200

    Refactoring to make function tail recursive

--- src/main/scala/cat/xampi/kata/prime/PrimeFactors.scala
+++ src/main/scala/cat/xampi/kata/prime/PrimeFactors.scala
@@ -1,15 +1,18 @@
 package cat.xampi.kata.prime
 
+import scala.annotation.tailrec
+
 object PrimeFactors {
   def generate(number: Int): List[Int] = {
-    def generateFactorsRecursive(number: Int, divider: Int): List[Int] = {
+    @tailrec
+    def generateFactorsRecursive(number: Int, divider: Int, acc: List[Int]): List[Int] = {
       number match {
         case 1 => List.empty
-        case x if number % divider == 0 => divider :: generateFactorsRecursive(number / divider, 2)
-        case _ => generateFactorsRecursive(number, divider + 1)
+        case x if number % divider == 0 => generateFactorsRecursive(number / divider, 2, divider :: acc)
+        case _ => generateFactorsRecursive(number, divider + 1, acc)
       }
     }
 
-    generateFactorsRecursive(number, 2)
+    generateFactorsRecursive(number, 2, List.empty)
   }
 }
