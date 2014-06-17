75b5675e4135a613c1f59d1243f4dea2ffb80afa
Josep Maria Camps i Riba <jmcamps@gmail.com>
2014-06-17 15:47:59 +0200

    Int extended to offer isDivisibleBy method. For readability ;-)

--- src/main/scala/cat/xampi/kata/prime/PrimeFactors.scala
+++ src/main/scala/cat/xampi/kata/prime/PrimeFactors.scala
@@ -3,16 +3,20 @@ package cat.xampi.kata.prime
 import scala.annotation.tailrec
 
 object PrimeFactors {
+  implicit class ExtendedInt(val value: Int) extends AnyVal {
+	  def isDivisibleBy(other:Int) = value % other == 0
+  }
+  
   def generate(number: Int): List[Int] = {
     @tailrec
     def generateFactorsRecursive(number: Int, divider: Int, acc: List[Int]): List[Int] = {
       number match {
         case 1 => acc.reverse
-        case x if number % divider == 0 => generateFactorsRecursive(number / divider, 2, divider :: acc)
+        case x if number isDivisibleBy divider => generateFactorsRecursive(number / divider, 2, divider :: acc)
         case _ => generateFactorsRecursive(number, divider + 1, acc)
       }
-    }
-
-    generateFactorsRecursive(number, 2, List.empty) 
+    }    
+        
+    generateFactorsRecursive(number, 2, List.empty)
   }
 }
